6c642f0406a1feef61de15e5f9cf8572a2e298b1
Real-Kami <67558858+Real-Kami@users.noreply.github.com>
2023-10-03 19:53:40 +0200

    Update main.py
    changes about window's position, bouncing system and moved toward a more SRP approach

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-import pygame, sys
-from pygame.locals import *
-from random import *
-from math import *
+import pygame, sys          # pour le jeu et la gestion de la fermeture
+from random import *        # pour les rebonds aléatoires
+from math import *          # pour le calcul d'angle avec sinus cosinus
+from time import sleep      # pour le délai
+import os
+
+
+
+
 
 pygame.init()
-pygame.font.init()
+pygame.font.init() # initalise une police pour l'affichage de texte
 
-fenetre = pygame.display.set_mode((1000, 800))
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d, %d" %(20, 50)
+fenetre = pygame.display.set_mode((1000, 650))
 my_font = pygame.font.SysFont('Arial', 30)
 
-done = False
+done = False # condition de la game loop
+
+
+
+
 
 class Balle:
     def __init__(self):
-        self.pos = [400, 400]
+        self.pos = [fenetre.get_width() - 50, fenetre.get_height() / 2]
         self.vitesse = 1
-        a = random() * 180
-        self.vect = [cos(a), sin(a)]
+        self.a = 0
+        self.vect = [cos(self.a), sin(self.a)]
 
     def afficher(self):
         posround = [ int(round(self.pos[0])), int(round(self.pos[1])) ]
@@ -25,38 +35,36 @@ class Balle:
     def avancer(self):
         self.pos[0] += self.vect[0] * self.vitesse
         self.pos[1] += self.vect[1] * self.vitesse
+        self.rebondir()
 
-        if self.pos[0] > 1000:
+    def rebondir(self):
+        if self.pos[0] > fenetre.get_width():
             self.vect[0] = -self.vect[0]
 
+        if self.pos[1] < 0 or self.pos[1] > fenetre.get_height():
+            self.vect[1] = -self.vect[1]
+
+        self.gameover()
+
+    def gameover(self):
         if self.pos[0] < 0:
-            for i in range(len(raquettes)):
-                print("score du joueur " + str(i) + " : " + str(raquettes[i].score))
+            sleep(3)
             pygame.display.quit()
             pygame.quit()
             sys.exit()
 
-        if self.pos[1] < 0 or self.pos[1] > 800:
-            self.vect[1] = -self.vect[1]
-
     def collideCheck(self, raquette):
         rect = pygame.Rect( raquette.pos[0], raquette.pos[1], 10, 120 )
         if rect.collidepoint(self.pos[0], self.pos[1]):
+            self.rebondirRaquette(raquette)
 
-            raquette.score += 1
+    def rebondirRaquette(self, raquette):
+        raquette.score += 1
+        self.vitesse += 0.1
+        self.a = random() * 2 - 1
+        self.vect = [cos(self.a), sin(self.a)]
 
-            barycentre = [raquette.pos[0] + raquette.dim[0] / 2,raquette.pos[1] + raquette.dim[1] / 2 ]
-            centreSurface = [barycentre[0] + raquette.dim[0] / 2, barycentre[1]]
 
-            x = centreSurface[0] - barycentre[0]
-            y = centreSurface[1] - self.pos[1]
-
-            angle = degrees(atan2(y, x))
-
-            self.vect = [cos(angle), -sin(angle)]
-            self.vect[0] = abs(self.vect[0])
-
-            self.vitesse += 0.1
 
 class Raquette:
     def __init__(self, x):
@@ -70,8 +78,15 @@ class Raquette:
     def suivre(self):
         self.pos[1] = pygame.mouse.get_pos()[1]
 
+
+
+
 balle = Balle()
 raquettes = [Raquette(50)]
+
+
+
+sleep(1)
 
 while done == False :
 
@@ -81,21 +96,17 @@ while done == False :
             done = True
 
     fenetre.fill([50, 50, 70])
-    fenetre.set_alpha(10)
+
+    text_surface = my_font.render('Score : ' + str(raquettes[0].score), False, (255, 255, 255))
+    fenetre.blit(text_surface, (20,20))
 
     raquettes[0].suivre()
     raquettes[0].afficher()
 
     balle.collideCheck(raquettes[0])
-
     balle.avancer()
     balle.afficher()
-
-    text_surface = my_font.render('Score : ' + str(raquettes[0].score), False, (255, 255, 255))
-    fenetre.blit(text_surface, (20,20))
 
     pygame.display.flip()
 
 pygame.quit()
-
-#copyright Real-Kami 03/10/2023
